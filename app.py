24e5298d59bd37b08db24935008fc7e14f879d42
saifurrehmanawan <145745643+saifurrehmanawan@users.noreply.github.com>
2024-07-22 14:15:14 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,31 @@
 from pytube import YouTube
 import streamlit as st
+from langcodes import Language
 
-# Title of the app
-st.title("_____________________")
+# Get a list of all language codes
+def get_all_languages():
+    languages = {}
+    for lang in Language.list():
+        languages[lang.language] = lang.language
+    return languages
 
-# Text input for YouTube URL
-youtube_url = st.text_input("Enter YouTube Video URL:")
+# Streamlit app
+def main():
+    
+    # Title of the app
+    st.title("_____________________")
 
-# Check if a URL has been provided
-if youtube_url:
-    yt = YouTube(youtube_url)
-    st.subheader(yt.title)
+    # Text input for YouTube URL
+    youtube_url = st.text_input("Enter YouTube Video URL:")
+
+    # Create toggle list for languages
+    selected_languages = []
+    for language_name, language_code in languages.items():
+        if st.checkbox(f'Select {language_name}', value=False):
+            selected_languages.append(language_code)
+
+    # Check if a URL has been provided
+    if youtube_url:
+        yt = YouTube(youtube_url)
+        st.subheader(yt.title)
+    
